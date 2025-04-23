28921c7e364427ff8cacc15812a6c94f8f4284c4
Winston Alexander <winstona2008@icloud.com>
2025-04-23 15:58:09 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -55,6 +55,72 @@ public class Main
 		System.out.println();
 	}
 	
+	public static boolean isBlocked(String[][] coordinatesArray, int currentX, int currentY)
+	{
+        // checking if player is blocked
+		boolean blockedUp = false;
+        if (currentY != 0)
+        {
+            if (!coordinatesArray[currentX][currentY - 1].equals(" "))
+                blockedUp = true;
+        }
+        else
+            blockedUp = true;
+        boolean blockedDown = false;
+        if (currentY != 9)
+        {
+            if (!coordinatesArray[currentX][currentY + 1].equals(" "))
+                blockedDown = true;
+        }
+        else
+            blockedDown = true;
+        boolean blockedLeft = false;
+        if (currentX != 0)
+        {
+            if (!coordinatesArray[currentX - 1][currentY].equals(" "))
+                blockedLeft = true;
+        }
+        else
+            blockedLeft = true;
+        boolean blockedRight = false;
+        if (currentX != 9)
+        {
+            if (!coordinatesArray[currentX + 1][currentY].equals(" "))
+                blockedRight = true;
+        }
+        else
+            blockedRight = true;
+        if (blockedUp && blockedDown && blockedLeft && blockedRight)
+            return true;
+        return false;
+	}
+	
+	public static String[][] generateBarriers(int numOfBarriers, String[][] coordinatesArray)
+	{
+	    int i = 0;
+	    while (i < numOfBarriers)
+	    {
+	        int randX = (int) (Math.random()*8 + 1);
+	        int randY = (int) (Math.random()*8 + 1);
+	        if (coordinatesArray[randX][randY].equals(" "))
+	        {
+	            coordinatesArray[randX][randY] = ".";
+    	        i++;
+	        }
+	    }
+		return coordinatesArray;
+	}
+	
+	public static void printScreen(String[][] coordinatesArray)
+	{
+	    
+        // clearing screen
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+		// printing
+		printArray(coordinatesArray);
+	}
+	
 	public static void main(String[] args)
 	{
 	    // creating coordinates array
@@ -64,101 +130,70 @@ public class Main
 		// spawning players
 		coordinatesArray[0][0] = "1";
 		coordinatesArray[9][9] = "2";
-		// adding random barrier
-		coordinatesArray[(int) (Math.random()*8 + 1)][(int) (Math.random()*8 + 1)] = ".";
-		
+		// adding three random barriers
+		coordinatesArray = generateBarriers(3, coordinatesArray);
 		int turn = 0;
-		
 		boolean run = true;
-
 		Scanner scanner = new Scanner(System.in);
-		
 		while (run)
 		{
 		    // initializing player variables based on turn
 			String player;
 			String otherPlayer;
-			String playerMarker;
-			String otherPlayerMarker;
 			if (turn%2 == 0)
 			{
 			    player = "1";
 			    otherPlayer = "2";
-			    playerMarker = ".";
-			    otherPlayerMarker = ".";
 			}
 			else
 			{
 			    player = "2";
 			    otherPlayer = "1";
-			    playerMarker = ".";
-			    otherPlayerMarker = ".";
 			}
 			// getting current coordinates of player
-		    int currentX = -1;
-		    int currentY = -1;
-		    for (int i = 0; i < coordinatesArray.length; i++)
-			    for (int j = 0; j < coordinatesArray[i].length; j++)
-			        if (coordinatesArray[i][j].equals(player))
-			        {
-			            currentX = i;
-			            currentY = j;
-			        }
-	        // checking if player is blocked
-			boolean blockedUp = false;
-            if (currentY != 0)
+    	    int currentX = -1;
+    	    int currentY = -1;
+    	    for (int i = 0; i < coordinatesArray.length; i++)
+    		    for (int j = 0; j < coordinatesArray[i].length; j++)
+    		        if (coordinatesArray[i][j].equals(player))
+    		        {
+    		            currentX = i;
+    		            currentY = j;
+    		        }
+			// getting current coordinates of other player
+    		int otherPlayerCurrentX = -1;
+    	    int otherPlayerCurrentY = -1;
+    	    for (int i = 0; i < coordinatesArray.length; i++)
+    		    for (int j = 0; j < coordinatesArray[i].length; j++)
+    		        if (coordinatesArray[i][j].equals(otherPlayer))
+    		        {
+    		            otherPlayerCurrentX = i;
+    		            otherPlayerCurrentY = j;
+    		        }
+			if (isBlocked(coordinatesArray, currentX, currentY))
             {
-                if (!coordinatesArray[currentX][currentY - 1].equals(" "))
-                    blockedUp = true;
-            }
-            else
-                blockedUp = true;
-            boolean blockedDown = false;
-            if (currentY != 9)
-            {
-                if (!coordinatesArray[currentX][currentY + 1].equals(" "))
-                    blockedDown = true;
-            }
-            else
-                blockedDown = true;
-            boolean blockedLeft = false;
-            if (currentX != 0)
-            {
-                if (!coordinatesArray[currentX - 1][currentY].equals(" "))
-                    blockedLeft = true;
-            }
-            else
-                blockedLeft = true;
-            boolean blockedRight = false;
-            if (currentX != 9)
-            {
-                if (!coordinatesArray[currentX + 1][currentY].equals(" "))
-                    blockedRight = true;
-            }
-            else
-                blockedRight = true;
-            if (blockedUp && blockedDown && blockedLeft && blockedRight)
-            {
-        		System.out.print("\033[H\033[2J");
-        		System.out.flush();
-    			printArray(coordinatesArray);
+        		printScreen(coordinatesArray);
                 System.out.println("Player " + otherPlayer + " wins!");
                 run = false;
             }
+            else if (isBlocked(coordinatesArray, otherPlayerCurrentX, otherPlayerCurrentY))
+            {
+        		printScreen(coordinatesArray);
+                System.out.println("Player " + player + " wins!");
+                run = false;
+            }
+            
             if (run)
             {
-                // clearing screen
-        		System.out.print("\033[H\033[2J");
-        		System.out.flush();
-        		// printing
-    			printArray(coordinatesArray);
+        		printScreen(coordinatesArray);
     		    System.out.println("Player " + player + "'s turn");
     			System.out.println("Enter coordinates as 'x y'");
     			String coordinates = scanner.nextLine();
     			int x;
     			int y;
-    			// checking for valid user input
+    			// checking for valid user input length
     			if (coordinates.length() == 3)
+        			// checking for valid user input formatting
     				if (coordinates.substring(1, 2).equals(" "))
     				{
     				    // getting x and y
@@ -183,7 +218,7 @@ public class Main
                     			        if (!blocked)
                     			        {
             					            for (int i = y; i < currentY; i++)
-                        			            coordinatesArray[x][i + 1] = playerMarker;
+                        			            coordinatesArray[x][i + 1] = ".";
                         					coordinatesArray[x][y] = player;
                                 			turn++;
                     			        }
@@ -197,7 +232,7 @@ public class Main
                     			        if (!blocked)
                     			        {
             					            for (int i = currentY; i < y; i++)
-                        			            coordinatesArray[x][i] = playerMarker;
+                        			            coordinatesArray[x][i] = ".";
                         					coordinatesArray[x][y] = player;
                                 			turn++;
                     			        }
@@ -214,7 +249,7 @@ public class Main
                     			        if (!blocked)
                     			        {
             					            for (int i = x; i < currentX; i++)
-                        			            coordinatesArray[i + 1][y] = playerMarker;
+                        			            coordinatesArray[i + 1][y] = ".";
                         					coordinatesArray[x][y] = player;
                                 			turn++;
                     			        }
@@ -228,7 +263,7 @@ public class Main
                     			        if (!blocked)
                     			        {
             					            for (int i = currentX; i < x; i++)
-                        			            coordinatesArray[i][y] = playerMarker;
+                        			            coordinatesArray[i][y] = ".";
                         					coordinatesArray[x][y] = player;
                                 			turn++;
                     			        }
